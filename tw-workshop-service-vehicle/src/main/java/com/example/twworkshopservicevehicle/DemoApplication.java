905ffb470969e8a41279cc41698b331a080ec4b1
dujie <jiedu@thoughtworks.com>
2017-12-19 11:01:15 +0800

    [N/A] - remove unused package

--- tw-workshop-service-vehicle/src/main/java/com/example/twworkshopservicevehicle/DemoApplication.java
+++ tw-workshop-service-vehicle/src/main/java/com/example/twworkshopservicevehicle/DemoApplication.java
@@ -1,7 +1,6 @@
 package com.example.twworkshopservicevehicle;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 
 @SpringCloudApplication
